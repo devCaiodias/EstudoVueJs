530024fc4dd959dff2b0664fd296898275a5d540
devCaiodias <caiodiasmartins12@gmail.com>
2024-08-30 16:58:41 -0300

    Atributos dinâmicos

---
+++ install-vue/vue-project/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="avatar" :alt="descrition" width="200px">
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
+        name: 'Picture',
+        data() {
+            return {
+                avatar: '/img/best.jpg',
+                descrition: 'Vagabond'
+            }
+        }
+    }
+</script>
--- install-vue/vue-project/src/components/info.vue
+++ install-vue/vue-project/src/components/info.vue
@@ -10,18 +10,25 @@
             <li>Vue</li>
         </ul>
         <p v-show="mostrar_email">Mande essa Mensagem para: {{ email}}</p>
+        <p>Para ver meu portifolio basta <a v-bind:href="meuLink">acessar aki</a></p>
+        <Picture/>
     </div>
 </template>
 
 <script>
+    import Picture from './Picture.vue'
+    
     export default {
+  // eslint-disable-next-line vue/no-reserved-component-names, vue/no-unused-components
+        components: { Picture },
         // eslint-disable-next-line vue/multi-word-component-names
         name: 'info',
         data() {
             return {
                 esta_Trabalhando: false,
                 mostrar_email: true,
-                email: 'caiodias@gmail.com'
+                email: 'caiodias@gmail.com',
+                meuLink: 'https://www.lofi.com/'
             }
         }
     }
