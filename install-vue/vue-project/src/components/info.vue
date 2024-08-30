2e58976ac8cf11d99ee54ee8f2c3f7adb9593013
devCaiodias <caiodiasmartins12@gmail.com>
2024-08-30 15:50:54 -0300

    Conhecendo as diretivas (v-if, v-show, v-for)

--- install-vue/vue-project/src/components/info.vue
+++ install-vue/vue-project/src/components/info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>Estou Trabalhando no momento.</p>
+        <p v-if="esta_Trabalhando">Estou Trabalhando no momento.</p>
+        <p v-else>Mais Esto estudando no momento.</p>
         <p>Utilizo as seguinte Limguagem: </p>
         <ul>
             <li>Html</li>
@@ -8,12 +9,20 @@
             <li>Js</li>
             <li>Vue</li>
         </ul>
+        <p v-show="mostrar_email">Mande essa Mensagem para: {{ email}}</p>
     </div>
 </template>
 
 <script>
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
-        name: 'info'
+        name: 'info',
+        data() {
+            return {
+                esta_Trabalhando: false,
+                mostrar_email: true,
+                email: 'caiodias@gmail.com'
+            }
+        }
     }
 </script>
