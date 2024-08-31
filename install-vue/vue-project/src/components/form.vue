54d1d1645b969c5f020b75867fda87ea17a38d15
devCaiodias <caiodiasmartins12@gmail.com>
2024-08-31 20:22:42 -0300

    Eventos (@submit e @click)

--- install-vue/vue-project/src/components/form.vue
+++ install-vue/vue-project/src/components/form.vue
@@ -1,28 +1,49 @@
 <template>
     <div>
         <h2>Meu Formulario:</h2>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <div>
-                <InputText />
+                <input type="text" v-model="name">
             </div>
             <div>
-                <InputText />
+                <input type="text" v-model="email">
             </div>
             <Submit />
         </form>
+        <p>Conteudo</p>
     </div>
 </template>
 
 <script>
-import InputText from './form/inputText.vue';
+// import InputText from './form/inputText.vue';
 import Submit from './form/Submit.vue';
 
     export default {
         // eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
         name: 'Form',
         components: {
-            InputText,
+            // InputText,
             Submit
+        },
+        data() {
+            return {
+                name: '',
+                email: ''
+            }
+        },
+        methods: {
+            enviarFormulario(e) {
+                e.preventDefault();
+
+                const name = this.name
+                const email = this.email
+                
+                console.log(`Dados enviados Nome =${name} e Email =${email}`)
+
+                // Ajax
+
+                // Inserir no banco de dados
+            }
         }
     }
 </script>
