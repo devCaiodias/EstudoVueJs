2d6687070a3fb5d5e49e1105de1b3821f6e8ffb5
devCaiodias <caiodiasmartins12@gmail.com>
2024-09-01 11:08:20 -0300

    Múltiplos eventos

---
+++ install-vue/vue-project/src/components/MultiplosEventos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1,$event), segundo(txt2, $event), terceiro($event)">Ativar multipos Eventos</button>
+        <p>{{ multiplos_event }}</p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'MultiplosEventos',
+        data() {
+            return {
+                multiplos_event: '',
+                txt1: 'Primeiro evento',
+                txt2: 'Segundo evento'
+            }
+        },
+        methods: {
+            primeiro(txt) {
+                this.multiplos_event = txt
+            },
+
+            segundo(txt) {
+                setTimeout(() => this.multiplos_event = txt, 2000)
+            },
+            terceiro() {
+                setTimeout(() => this.multiplos_event = "", 3000)
+            }
+        }
+    }
+    
+</script>
+
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
--- install-vue/vue-project/src/components/primeiroComponente.vue
+++ install-vue/vue-project/src/components/primeiroComponente.vue
@@ -3,12 +3,18 @@
         <h1>Olá Vue!</h1>
         <p> Meu nome é {{ nome }} </p>
         <p> minha Carreira é {{ Carreira }} </p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue'
+
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: "Matheus",
